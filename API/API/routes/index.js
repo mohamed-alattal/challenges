b7c8b6809a71c2912736e9840533e44735e2f2bd
mohamed-alattal <alattalmohamed@gmail.com>
2016-05-20 03:28:57 +0200

    the second api for getting products added

--- API/API/routes/index.js
+++ API/API/routes/index.js
@@ -9,12 +9,48 @@ database.connect(function(err,client){
   console.log("databes is successfully connected");
 });
 
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// endpoint to store a new price entry
+router.post('/api/storeprice',function(req,res){
+   var url= req.body.url;
+   var price= req.body.price;
 
+  database.db().query('SELECT * FROM products WHERE url= $1',[url],function(err,result){
+    if(err || result.rows.length==0) res.send('no data mach the url'+' '+result.rows.length+' '+err);
+
+    else{
+
+    var pid= result.rows[0].id;
+    console.log(pid);
+    database.db().query('INSERT INTO prices VALUES ($1,$2,clock_timestamp(),$3,$4)',[1002,pid,parseFloat(price),price[price.length-1]],function(err,resu){
+      if(err) res.send('insertion failed'+' '+err);
+      else
+      res.send('insertion successful');
+    });
+  }
+  });
+});
+// endpoint for displaying list of products of a store
+router.get('/api/store/:url',function(req,res){
+  database.db().query('select * from stores where url=$1',[req.params.url],function(err,result){
+    if(err) res.send('no store matches that url');
+    else{
+      var sid= result.rows[0].id;
+      database.db().query('select p.id,max(ps.value),min(ps.value),count(ps.value) as "count" from products p inner join prices ps ON (p.id=ps.products_id) where stores_id=$1 GROUP BY p.id',[sid],function(err,result){
+        if(err) res.send('no products for this store');
+        else{
+          res.send(result);
+        }
+      });
+    }
+  });
+});
 
 
 module.exports = router;
